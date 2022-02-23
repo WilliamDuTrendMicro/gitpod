5b08d8ac465293ea5d2722a031306cd9dea9b2e9
Mads Hartmann <mads379@gmail.com>
2022-02-23 10:42:30 +0000

    WIP: Curl echo server
    /werft with-vm=true

--- .werft/vm/manifests.ts
+++ .werft/vm/manifests.ts
@@ -41,14 +41,6 @@ spec:
       labels:
         harvesterhci.io/vmName: ${vmName}
     spec:
-      readinessProbe:
-        tcpSocket:
-          port: 2200
-        initialDelaySeconds: 120
-        periodSeconds: 20
-        timeoutSeconds: 10
-        failureThreshold: 10
-        successThreshold: 10
       domain:
         hostname: ${vmName}
         machine:
@@ -149,13 +141,13 @@ type UserDataSecretManifestOptions = {
 export function UserDataSecretManifest({vmName, namespace, secretName }: UserDataSecretManifestOptions) {
   const userdata = Buffer.from(`#cloud-config
 users:
-- name: ubuntu
+- name: mads
   sudo: "ALL=(ALL) NOPASSWD: ALL"
   ssh_authorized_keys:
     - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/aB/HYsb56V0NBOEab6j33v3LIxRiGqG4fmidAryAXevLyTANJPF8m44KSzSQg7AI7PMy6egxQp/JqH2b+3z1cItWuHZSU+klsKNuf5HxK7AOrND3ahbejZfyYewtKFQ3X9rv5Sk8TAR5gw5oPbkTR61jiLa58Sw7UkhLm2EDguGASb6mBal8iboiF8Wpl8QIvPmJaGIOY2YwXLepwFA3S3kVqW88eh2WFmjTMre5ASLguYNkHXjyb/TuhVFzAvphzpl84RAaEyjKYnk45fh4xRXx+oKqlfKRJJ/Owxa7SmGO+/4rWb3chdnpodHeu7XjERmjYLY+r46sf6n6ySgEht1xAWjMb1uqZqkDx+fDDsjFSeaN3ncX6HSoDOrphFmXYSwaMpZ8v67A791fuUPrMLC+YMckhTuX2g4i3XUdumIWvhaMvKhy/JRRMsfUH0h+KAkBLI6tn5ozoXiQhgM4SAE5HsMr6CydSIzab0yY3sq0avmZgeoc78+8PKPkZG1zRMEspV/hKKBC8hq7nm0bu4IgzuEIYHowOD8svqA0ufhDWxTt6A4Jo0xDzhFyKme7KfmW7SIhpejf3T1Wlf+QINs1hURr8LSOZEyY2SzYmAoQ49N0SSPb5xyG44cptpKcj0WCAJjBJoZqz0F5x9TjJ8XToB5obyJfRHD1JjxoMQ== dev@gitpod.io
 chpasswd:
   list: |
-    ubuntu:ubuntu
+    mads:mads
   expire: False
 write_files:
   - path: /etc/ssh/sshd_config.d/101-change-ssh-port.conf
@@ -167,51 +159,8 @@ write_files:
     owner: root
     content: |
       #!/bin/bash
-
       set -eo pipefail
-
-      # inspired by https://github.com/gitpod-io/ops/blob/main/deploy/workspace/templates/bootstrap.sh
-
-      # Install k3s
-      export INSTALL_K3S_SKIP_DOWNLOAD=true
-
-      /usr/local/bin/install-k3s.sh \\
-          --token "1234" \\
-          --node-ip "$(hostname -I | cut -d ' ' -f1)" \\
-          --node-label "cloud.google.com/gke-nodepool=control-plane-pool" \\
-          --container-runtime-endpoint=/var/run/containerd/containerd.sock \\
-          --write-kubeconfig-mode 444 \\
-          --disable traefik \\
-          --disable metrics-server \\
-          --flannel-backend=none \\
-          --kubelet-arg config=/etc/kubernetes/kubelet-config.json \\
-          --kubelet-arg feature-gates=LocalStorageCapacityIsolation=true \\
-          --kubelet-arg feature-gates=LocalStorageCapacityIsolationFSQuotaMonitoring=true \\
-          --kube-apiserver-arg feature-gates=LocalStorageCapacityIsolation=true \\
-          --kube-apiserver-arg feature-gates=LocalStorageCapacityIsolationFSQuotaMonitoring=true \\
-          --cluster-init
-
-      kubectl label nodes ${vmName} \\
-          gitpod.io/workload_meta=true \\
-          gitpod.io/workload_ide=true \\
-          gitpod.io/workload_workspace_services=true \\
-          gitpod.io/workload_workspace_regular=true \\
-          gitpod.io/workload_workspace_headless=true \\
-          gitpod.io/workspace_0=true \\
-          gitpod.io/workspace_1=true \\
-          gitpod.io/workspace_2=true
-
-      # apply fix from https://github.com/k3s-io/klipper-lb/issues/6 so we can use the klipper servicelb
-      # this can be removed if https://github.com/gitpod-io/gitpod-packer-gcp-image/pull/20 gets merged
-      cat /var/lib/gitpod/manifests/calico.yaml | sed s/__KUBERNETES_NODE_NAME__\\"\\,/__KUBERNETES_NODE_NAME__\\",\\ \\"container_settings\\"\\:\\ \\{\\ \\"allow_ip_forwarding\\"\\:\\ true\\ \\}\\,/ > /var/lib/gitpod/manifests/calico2.yaml
-      kubectl apply -f /var/lib/gitpod/manifests/calico2.yaml
-
-      kubectl apply -f /var/lib/gitpod/manifests/cert-manager.yaml
-      kubectl apply -f /var/lib/gitpod/manifests/metrics-server.yaml
-
-      cat <<EOF >> /etc/bash.bashrc
-      export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-      EOF
+      curl https://58000-gitpodio-gitpod-g8bmoug3qwh.ws-eu33.gitpod.io
 runcmd:
  - bash /usr/local/bin/bootstrap-k3s.sh`).toString("base64")
   return `
