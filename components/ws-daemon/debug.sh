2387174644e7cf644bca5d1750ca40d360865971
Sven Efftinge <sven@gitpod.io>
2022-02-24 15:20:06 +0000

    [multi-repo] Support prebuilds on multi-repo setup

--- components/ws-daemon/debug.sh
+++ components/ws-daemon/debug.sh
@@ -11,8 +11,8 @@
 docker ps &> /dev/null || (echo "You need a working Docker daemon. Maybe set DOCKER_HOST?"; exit 1)
 gcloud auth list | grep typefox &>/dev/null || (echo "Login using 'gcloud auth login' for the docker push to work"; exit 1)
 
-leeway build .:docker -Dversion=dev
-devImage=eu.gcr.io/gitpod-dev/ws-daemon:dev
+leeway build .:docker -Dversion=dev -DimageRepoBase=eu.gcr.io/gitpod-core-dev/build
+devImage=eu.gcr.io/gitpod-core-dev/build/ws-daemon:dev
 
 kubectl patch daemonset ws-daemon --patch '{"spec": {"template": {"spec": {"containers": [{"name": "ws-daemon","image": "'$devImage'"}]}}}}'
 kubectl get pods --no-headers -o=custom-columns=:metadata.name | grep ws-daemon | xargs kubectl delete pod
